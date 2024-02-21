c58d0e267802b7e4827b54c7deed201049b7fcfa
caleb <steppcb@mail.uc.edu>
2024-02-21 14:04:35 -0500

    Initial commit

---
+++ Get_loopy/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ctr = 10; //starting variable
+
+        while (ctr >= 0) {
+            System.out.println(ctr);
+            ctr = ctr - 2;
+        }
+    }
+}
